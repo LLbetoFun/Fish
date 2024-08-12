5c63296fd3e790964fd8b06a7ff0ea27bcd497b0
MSI <bbt5xh@Hotmail.com>
2024-08-12 10:13:47 +0800

    KeepSprint+

--- Injections/1.8.9/Injection1_8_9/src/com/fun/client/mods/movement/KeepSprint.java
+++ Injections/1.8.9/Injection1_8_9/src/com/fun/client/mods/movement/KeepSprint.java
@@ -5,21 +5,28 @@ import com.fun.eventapi.event.events.EventUpdate;
 import org.lwjgl.input.Keyboard;
 import com.fun.client.mods.Category;
 import com.fun.client.mods.Module;
-public class KeepSprint extends Module {
-    public KeepSprint(String nameIn) {
-        super(Keyboard.KEY_R,nameIn, Category.Movement);
-    }
-    public Setting setting=new Setting("Test",this,false){
-        @Override
-        public boolean isVisible() {
-            return !running;
-        }
-    };
 
+import java.util.Random;
+
+public class KeepSprint extends Module {
+    public Setting changeChanceSetting;
+    public KeepSprint(String nameIn) {
+        super(Keyboard.KEY_R, nameIn, Category.Movement);
+        this.changeChanceSetting = new Setting("Chance", this, 50.0, 0.0, 100.0, false);
+    }
     @Override
     public void onUpdate(EventUpdate event) {
         super.onUpdate(event);
-        //Agent.System.out.println("update");
-        mc.getPlayer().setSprinting(true);
+        // 检查sprint
+        if (!mc.getPlayer().isSprinting() && mc.getPlayer().isMoving()) {
+            if (shouldChangeSprint()) {
+                mc.getPlayer().setSprinting(true);
+            }
+        }
+    }
+    private boolean shouldChangeSprint() {
+        Random random = new Random();
+        double chance = changeChanceSetting.getValDouble();
+        return random.nextDouble() * 100 < chance;
     }
 }
